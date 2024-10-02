3a9f4e1dd51ecb789e10664020e98adf817baac5
denizberkin <berkindeniz2000@gmail.com>
2024-10-02 19:31:41 +0300

    add utils folder

---
+++ utils/simple_classifier.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)  # in_ch, out_ch, filter_size in order
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = torch.max_pool2d(torch.relu(self.conv1(x)), 2)
+        x = torch.max_pool2d(torch.relu(self.conv2(x)), 2)
+        x = x.view(-1, 32 * 8 * 8)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
